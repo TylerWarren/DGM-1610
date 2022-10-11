d981714cd5c3482e6e366c09bce40853c160e5c8
Tyler Warren <10937681@my.uvu.edu>
2022-10-11 17:43:25 -0600

    created text label

---
+++ ScriptableObject/Assets/TextLabelBehaviour.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+
+public class TextLabelBehaviour : MonoBehaviour
+{
+    public Text label;
+    public FloatData dataObj
+
+    private void Start()
+    {
+        label = GetComponant<Text>();
+        label.text = dataObj.value.ToString(CultureInfo.InvariantCulture);
+    }
+}
