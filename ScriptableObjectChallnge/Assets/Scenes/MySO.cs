463a382607ce8ff0e0d4167ae5d9aaa068e96c97
Tyler Warren <10937681@my.uvu.edu>
2022-10-13 17:01:10 -0600

    created more pratice code

---
+++ ScriptableObjectChallnge/Assets/Scenes/MySO.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class MySO : ScriptableObject
+{
+int a = 10;
+    int[] b = new int[5] {0, 17, 34, 42, 67};
+
+    public int A
+    {
+        get {return a; }
+    }
+
+    // return value in b array, or -1 if x is out-of-range
+    public int B(int x)
+    {
+        if (x >= 0 && x <= 5)
+            return b[x];
+        else
+            return -1;
+    }
+
+}
