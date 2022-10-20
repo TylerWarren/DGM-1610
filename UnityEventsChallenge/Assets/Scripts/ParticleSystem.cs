9aea24d25ad4b6e089ecb8c6130adfaa3a73b439
Tyler Warren <10937681@my.uvu.edu>
2022-10-20 10:49:02 -0600

    Created Particle event

---
+++ UnityEventsChallenge/Assets/Scripts/ParticleSystem.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ParticleSystem : MonoBehaviour
+{
+   void OnParticleTrigger()
+    {
+        ParticleSystem ps = GetComponent<ParticleSystem>();
+
+    
+        List<ParticleSystem.Particle> enter = new List<ParticleSystem.Particle>();
+        List<ParticleSystem.Particle> exit = new List<ParticleSystem.Particle>();
+
+       
+        int numEnter = ps.GetTriggerParticles(ParticleSystemTriggerEventType.Enter, enter);
+        int numExit = ps.GetTriggerParticles(ParticleSystemTriggerEventType.Exit, exit);
+
+        
+        for (int i = 0; i < numEnter; i++)
+        {
+            ParticleSystem.Particle p = enter[i];
+            p.startColor = new Color32(255, 0, 0, 255);
+            enter[i] = p;
+        }
+        for (int i = 0; i < numExit; i++)
+        {
+            ParticleSystem.Particle p = exit[i];
+            p.startColor = new Color32(0, 255, 0, 255);
+            exit[i] = p;
+        }
+
+    
+        ps.SetTriggerParticles(ParticleSystemTriggerEventType.Enter, enter);
+        ps.SetTriggerParticles(ParticleSystemTriggerEventType.Exit, exit);
+    }
+}
