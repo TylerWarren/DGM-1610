b5aa39dcdc8ec5a1a216404db8504bc816aab942
Tyler Warren <10937681@my.uvu.edu>
2022-10-20 10:52:13 -0600

    Update

--- UnityEventsChallenge/Assets/Scripts/ParticleSystem.cs
+++ UnityEventsChallenge/Assets/Scripts/ParticleSystem.cs
@@ -22,14 +22,7 @@ public class ParticleSystem : MonoBehaviour
             p.startColor = new Color32(455, 2, 0, 095);
             enter[i] = p;
         }
-        for (int i = 0; i < numExit; i++)
-        {
-            ParticleSystem.Particle p = exit[i];
-            p.startColor = new Color32(0, 255, 0, 185);
-            exit[i] = p;
-        }
-
-    
+  
         ps.SetTriggerParticles(ParticleSystemTriggerEventType.Enter, enter);
         ps.SetTriggerParticles(ParticleSystemTriggerEventType.Exit, exit);
     }
