a5f2aff4a5e0523fa172e2be1dfbadf178c4356c
Tyler Warren <10937681@my.uvu.edu>
2022-10-26 11:58:07 -0600

    updated

--- UnityProjects/UFO_Defense_Force/Assets/Scripts_/PlayerController.cs
+++ UnityProjects/UFO_Defense_Force/Assets/Scripts_/PlayerController.cs
@@ -8,7 +8,7 @@ public class PlayerController : MonoBehaviour
     public float speed;
 
     
-     // Start is called before the first frame update
+    // Start is called before the first frame update
     void Start()
     {
         
