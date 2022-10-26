20ad75abde5f26f55495d373c549b2d98ccb605a
Tyler Warren <10937681@my.uvu.edu>
2022-10-26 11:55:49 -0600

    updated

--- UnityProjects/UFO_Defense_Force/Assets/Scripts_/PlayerController.cs
+++ UnityProjects/UFO_Defense_Force/Assets/Scripts_/PlayerController.cs
@@ -17,7 +17,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Set horizontalInput to recive values from keybord
+      // Set horizontalInput to recive values from keybord
        horizontalInput = Input.GetAxis("Horizantal");
 
     // Moves Player left and right
