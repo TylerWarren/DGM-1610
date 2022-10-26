85862126fd2d4bda4ddb5d2dbe27c9bdfbce1998
Tyler Warren <10937681@my.uvu.edu>
2022-10-26 15:17:46 -0600

    fixed errors

--- UnityProjects/UFO_Defense_Force/Assets/Scripts/DestroyOutOfBounds.cs
+++ UnityProjects/UFO_Defense_Force/Assets/Scripts/DestroyOutOfBounds.cs
@@ -2,28 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DestroyOutOfBonds : MonoBehaviour
+public class DestroyOutOfBounds : MonoBehaviour
 {
-    public float topBounds = 30.0f;
-    public float lowerBounds = -10.0f;
-
-
-    void Awake()
+    // Start is called before the first frame update
+    void Start()
     {
-        //Time.timeScale = 1;
+        
     }
+
     // Update is called once per frame
     void Update()
     {
-        if(transform.position.z < topBounds)
-        {
-            Destroy(gameObject);
-        }
-        else if(transform.position.z < lowerBounds)
-        {
-            Debug.Log("Game Over!");
-            Destroy(gameObject);
-             //Time.timeScale = 0;
-        }
+        
     }
 }
