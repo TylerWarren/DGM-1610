1728bcbac20b021f2cc27096df0ae565ec02115b
Tyler Warren <10937681@my.uvu.edu>
2022-10-26 15:25:57 -0600

    Did the Unit 6a Lab.

--- UnityProjects/UFO_Defense_Force/Assets/Scripts/DestroyOutOfBounds.cs
+++ UnityProjects/UFO_Defense_Force/Assets/Scripts/DestroyOutOfBounds.cs
@@ -2,17 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DestroyOutOfBounds : MonoBehaviour
+public class DestroyOutOfBonds : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float topBounds = 30.0f;
+    public float lowerBounds = -10.0f;
 
+
+    void Awake()
+    {
+        //Time.timeScale = 1;
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        if(transform.position.z < topBounds)
+        {
+            Destroy(gameObject);
+        }
+        else if(transform.position.z < lowerBounds)
+        {
+            Debug.Log("Game Over!");
+            Destroy(gameObject);
+             //Time.timeScale = 0;
+        }
     }
 }
