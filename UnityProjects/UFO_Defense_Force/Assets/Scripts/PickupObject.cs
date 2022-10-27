cbaf69d3356333bf62287e570b5957fe70521324
Tyler Warren <10937681@my.uvu.edu>
2022-10-27 16:11:18 -0600

    updated

---
+++ UnityProjects/UFO_Defense_Force/Assets/Scripts/PickupObject.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupObject : MonoBehaviour
+{
+    void OnTriggerEnter(Collider collider) 
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            print ("Item picked up");
+            Destroy (gameObject)
+        }
+    }
+} 
+
