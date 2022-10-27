ed22d6cb805154310d82d173ea6d6dbe37f10ef0
Tyler Warren <10937681@my.uvu.edu>
2022-10-27 17:01:42 -0600

    fixed errors

--- UnityProjects/UFO_Defense_Force/Assets/Scripts/SpawnerScript.cs
+++ UnityProjects/UFO_Defense_Force/Assets/Scripts/SpawnerScript.cs
@@ -2,26 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnerScript : MonoBehaviour
-{
+public class SpawnerScript : MonoBehaviour {
+
     public GameObject spawnee;
     public bool stopSpawning = false;
     public float spawnTime;
     public float spawnDelay;
 
     // Use this for initialization
-    void Start () 
-
-    {
+    void Start () {
         InvokeReapeating("SpawnObject", spawnTime, spawnDelay);
     }
 
-    public void SpawnObject()
-
-    {
-        Instantiate(spawnee, transform.position, transfrom.rotation);
-        if(stopSpawning)
-        {
+    public void SpawnObject() {
+        Instantiate(spawnee, transform.position, transform.rotation);
+        if(stopSpawning) {
             CancelInvoke("SpawnObject");
         }
     }
