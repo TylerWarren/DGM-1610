d88a53781e012594640fc4514bfce3c4f856be61
Tyler Warren <10937681@my.uvu.edu>
2022-10-27 15:04:52 -0600

    fixed errors

--- UnityProjects/UFO_Defense_Force/Assets/Scripts/PickupDelate.cs
+++ UnityProjects/UFO_Defense_Force/Assets/Scripts/PickupDelate.cs
@@ -3,15 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PickupDelate : MonoBehaviour
+
 {
  void OnCollisionEnter(Collision collision) 
 
-    { if(collision.gameObject.name == "barrierdestroyer1")
-
-     { 
-        Destroy(barrier1); 
-     }
-
+    { 
+        if(collision.gameObject.name == "Pickup")
+        { 
+             Destroy(collision.gameObject); 
+        }
      } 
  } 
 
