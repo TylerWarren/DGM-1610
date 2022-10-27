c91d786fb04c784f09338c6097fc56e46dfb61d4
Tyler Warren <10937681@my.uvu.edu>
2022-10-27 15:47:43 -0600

    updated

--- UnityProjects/UFO_Defense_Force/Assets/Scripts/PickupDelate.cs
+++ UnityProjects/UFO_Defense_Force/Assets/Scripts/PickupDelate.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class PickupDelate : MonoBehaviour
 {
     void OnCollisionEnter(Collision collider) 
-     {
-         if(col.gameObject.tag == "Pickup")
+    {
+        if(col.gameObject.tag == "Pickup")
+        
          {
              Destroy(col.gameObject);
          }
-     }
+    }
 } 
 
