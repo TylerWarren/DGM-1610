872805b475b098d33da01d5902400a6c8dee70df
Tyler Warren <10937681@my.uvu.edu>
2022-10-27 19:50:30 -0600

    fixed errors

--- UnityProjects/UFO_Defense_Force/Assets/Scripts/PickupDelate.cs
+++ UnityProjects/UFO_Defense_Force/Assets/Scripts/PickupDelate.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PickupDelate : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider collision) 
+    private void OnTriggerEnter2D(Collider2D collision) 
     {
         if (collision.tag == "Player")
         {
