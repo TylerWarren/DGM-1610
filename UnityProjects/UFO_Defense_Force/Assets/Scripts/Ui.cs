e0738f43e6098c9b6a690f69b43cc934a797fced
Tyler Warren <10937681@my.uvu.edu>
2022-10-28 12:00:41 -0600

    created score text.

---
+++ UnityProjects/UFO_Defense_Force/Assets/Scripts/Ui.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Ui : MonoBehaviour
+{
+    public Text MyscoreText
+    private int ScoreNum;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        ScoreNum = 0;
+        MyscoreText.text = "Score : " + ScoreNum;
+
+    }
+
+    
+}
