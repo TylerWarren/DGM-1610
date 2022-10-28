13eca226f3cccadea4943e07cde816537411eebb
Tyler Warren <10937681@my.uvu.edu>
2022-10-27 19:00:19 -0600

    updated

--- UnityProjects/UFO_Defense_Force/Assets/Scripts/Spawner.cs
+++ UnityProjects/UFO_Defense_Force/Assets/Scripts/Spawner.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    public GameObject pickup
+    public GameObject pickup;
     public float delay = 2f;
     public float speed = 3f;
     float nextTimeToSpawn;
