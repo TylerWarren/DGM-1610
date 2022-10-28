ac6e511a64ae8687722a390e3172d962de5f5779
Tyler Warren <10937681@my.uvu.edu>
2022-10-27 18:56:38 -0600

    created spawner

---
+++ UnityProjects/UFO_Defense_Force/Assets/Scripts/Spawner.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+public GameObject pickup
+public float delay = 2f;
+public float speed = 3f;
+float nextTimeToSpawn;
+
+void Start()
+    {
+        nextTimeToSpawn = Time.time;
+    }
+
+void Update()
+    {
+        if (Time.time > nextTimeToSpawn)
+        {
+            nextTimeToSpawn = Time.time + delay;
+            GameObject go = Instantiate(itam, transfrom.position, Quaternion.identity);
+            go.AddComponent<Move>();
+            go.GetComponent<Move>().speed = speed;
+        }
+    }
+}
