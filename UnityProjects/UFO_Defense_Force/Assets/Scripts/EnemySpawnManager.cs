87e2f3469aa6f24097883f54813a5e3ec012dbfb
Tyler Warren <10937681@my.uvu.edu>
2022-11-02 12:45:43 -0600

    updated

--- UnityProjects/UFO_Defense_Force/Assets/Scripts/EnemySpawnManager.cs
+++ UnityProjects/UFO_Defense_Force/Assets/Scripts/EnemySpawnManager.cs
@@ -5,14 +5,18 @@ using UnityEngine;
 public class EnemySpawnManager : MonoBehaviour
 {
   public GameObject[] ufoPrefabs; // Array to store UFO ships
-  public int ufoIndex;
+  private float spawnRangex = 20f;
+  private float spawnPosz = 20f;
+
 
     // Update is called once per frame
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.S))
         {
-            Instantiate(ufoPrefabs[ufoIndex],new Vector3(0,0,15), ufoPrefabs[ufoIndex].transform.rotation);
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX,spawnRangeX),0,spawnPosZ);
+            int ufoIndex = Random.Range(0,ufoPrefabs.Length); // Picks a random UFO from the array
+            Instantiate(ufoPrefabs[ufoIndex],spawnPos, ufoPrefabs[ufoIndex].transform.rotation); // Spawms a indexed UFO from the array a random location on the X-Axis
         }
     }
 }
