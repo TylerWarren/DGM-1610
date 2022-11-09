79c868c46365dcc06ce8fa66fa3926b4c5d6ccaf
Tyler Warren <10937681@my.uvu.edu>
2022-11-09 15:23:45 -0700

    Created Game over script

---
+++ UnityProjects/UFO_Defense_Force/Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public bool isGameOver;
+    private GameObject gameOverText;
+    void Awake()
+    {
+        Time.timeScale = 1;
+        isGameOver = false;
+    }
+    void Start()
+    {
+        gameOverText = GameObject.Find("GameOverText");
+
+    }
+        // Update is called one per frame
+    void update()
+    {
+        if(isGameOver)
+        {
+            EndGame(); // Start EndGame method
+        }
+        else
+            gameOverText.gameObject.SetActive(false); // Keep UI Text Game Over hidden
+    }
+        public void EndGame()
+    {
+        gameOverText.gameObject.SetActive(true);
+        Time.timeScale = 0; // Freeze Time
+    }
+}
