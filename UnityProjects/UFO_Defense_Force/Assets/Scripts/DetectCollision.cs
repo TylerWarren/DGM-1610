312432c57fa34269cb9f4c3c44380051a99809f2
Tyler Warren <10937681@my.uvu.edu>
2022-11-11 13:06:23 -0700

    updated

--- UnityProjects/UFO_Defense_Force/Assets/Scripts/DetectCollision.cs
+++ UnityProjects/UFO_Defense_Force/Assets/Scripts/DetectCollision.cs
@@ -20,11 +20,10 @@ public class DetectCollision : MonoBehaviour
       scoreManager.IncreaseScore(scoreToGive); // Increase the Score 
       Destroy(gameObject); // Destroy this gameobject
       Destroy(other.gameObject); // Destroy the other gameobject it hits
-      explosionSound.PlayOneShot(explosion, 1.0f);
    }
 
    void Update()
    {
-      
+      explosionSound.PlayOneShot(explosion, 1.0f);
    }
 }
