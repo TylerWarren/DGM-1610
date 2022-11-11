6af46b8462925183d528ea0220639a6cfc61af00
Tyler Warren <10937681@my.uvu.edu>
2022-11-11 12:18:13 -0700

    fixed error

--- UnityProjects/UFO_Defense_Force/Assets/Scripts/DetectCollision.cs
+++ UnityProjects/UFO_Defense_Force/Assets/Scripts/DetectCollision.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class DetectCollision : MonoBehaviour
 {
    public ScoreManager scoreManager; // Store referance to score manager
+   public AudioClip explosion;
    private AudioSource explosionSound;
 
    public int scoreToGive;
