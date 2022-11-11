a55029a7c6433fdd15e40ba3ec98cb54eaf3c87d
Tyler Warren <10937681@my.uvu.edu>
2022-11-11 14:34:36 -0700

    updated

--- UnityProjects/UFO_Defense_Force/Assets/Scripts/DetectCollision.cs
+++ UnityProjects/UFO_Defense_Force/Assets/Scripts/DetectCollision.cs
@@ -4,18 +4,26 @@ using UnityEngine;
 
 public class DetectCollision : MonoBehaviour
 {
+   private AudioSource explosion;
    public ScoreManager scoreManager; // Store referance to score manager
+   public AudioSource explosionSound;
 
    public int scoreToGive;
 
    void Start()
    {
       scoreManager = GameObject.Find("ScoreManager").GetComponent<ScoreManager>(); // Find ScoreManager gameobject and reference ScoreManager script component
+      explosion = GetComponent<AudioSource>();
    }
    void OnTriggerEnter(Collider other) // Once the Trigger had been entered record collision in the argument variable "other"
    {
       scoreManager.IncreaseScore(scoreToGive); // Increase the Score 
       Destroy(gameObject); // Destroy this gameobject
       Destroy(other.gameObject); // Destroy the other gameobject it hits
+   }
+
+   void Update()
+   {
+      explosionSound.PlayOneShot(explosion, 1.0f);
    }
 }
