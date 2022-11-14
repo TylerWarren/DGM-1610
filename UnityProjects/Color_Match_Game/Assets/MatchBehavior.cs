1e097a1fc602383a414b51255fdc5f9417bb4be7
Tyler Warren <10937681@my.uvu.edu>
2022-11-14 15:59:26 -0700

    added null check

--- UnityProjects/Color_Match_Game/Assets/MatchBehavior.cs
+++ UnityProjects/Color_Match_Game/Assets/MatchBehavior.cs
@@ -3,10 +3,12 @@ using UnityEngine;
 public class MatchBehavior : MonoBehaviour
 {
     public ID idObj;
+    private ID otherID;
 
     private void OnTriggerEnter(Collider other)
     {
-        var otherID = other.GetComponent<IDContainerBehaviour>().idObj;
+        var tempObj = other.GetComponent<IDContainerBehaviour>().idObj;
+        if(tempObj != null) otherID = tempObj.idObj;
         if (otherID == idObj)
         {
             Debug.Log("Matched");
