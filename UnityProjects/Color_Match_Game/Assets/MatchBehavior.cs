4ab5a22556822eae679803bd4635f1e2a6f848db
Tyler Warren <10937681@my.uvu.edu>
2022-11-15 22:44:01 -0700

    updated

--- UnityProjects/Color_Match_Game/Assets/MatchBehavior.cs
+++ UnityProjects/Color_Match_Game/Assets/MatchBehavior.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class MatchBehavior : MonoBehaviour
 {
     public ID idObj;
+    public UnityEvent matchEvent, noMatchEvent;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -13,11 +15,11 @@ public class MatchBehavior : MonoBehaviour
         var otherID = tempObj.idObj;
         if (otherID == idObj)
         {
-            Debug.Log("Matched");
+            matchEvent.Invoke();
         }
         else
         {
-            Debug.Log("No Match");
+            noMatchEvent.Invoke();
         }
     }
 }
