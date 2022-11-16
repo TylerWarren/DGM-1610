772d3b0158e21432744a293756ddbdf8836899e7
Tyler Warren <10937681@my.uvu.edu>
2022-11-16 14:40:59 -0700

    fixed error

--- UnityProjects/Color_Match_Game/Assets/TextLabelBehavior.cs
+++ UnityProjects/Color_Match_Game/Assets/TextLabelBehavior.cs
@@ -1,7 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using Text;
 
 [RequireComponent(typeof(Text))]
 public class TextLabelBehavior : MonoBehaviour
