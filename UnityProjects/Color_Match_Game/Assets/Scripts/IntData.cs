e45a3545a25f732b3d6e09c86869c0ee49c33093
Tyler Warren <10937681@my.uvu.edu>
2022-11-16 21:22:50 -0700

    updated

--- UnityProjects/Color_Match_Game/Assets/Scripts/IntData.cs
+++ UnityProjects/Color_Match_Game/Assets/Scripts/IntData.cs
@@ -6,12 +6,24 @@ public class IntData : ScriptableObject
 
 {
     public int value;
-    
+
     public UnityEvent disableEvent;
 
     public void SetValue(int num)
     {
         value = num;
+    }
+
+    public void CompareValue(IntData obj)
+    {
+        if (value >= obj.value)
+        {
+
+        }
+        else
+        {
+            value = obj.value;
+        }
     }
 
     public void SetValue(IntData obj)
