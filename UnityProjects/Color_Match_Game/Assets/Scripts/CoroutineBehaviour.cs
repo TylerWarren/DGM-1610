433ef26fb42e5343483b94844b6862c0f4b05251
Tyler Warren <10937681@my.uvu.edu>
2022-11-23 10:42:47 -0700

    updated

--- UnityProjects/Color_Match_Game/Assets/Scripts/CoroutineBehaviour.cs
+++ UnityProjects/Color_Match_Game/Assets/Scripts/CoroutineBehaviour.cs
@@ -5,9 +5,9 @@ using UnityEngine.Events;
 
 public class CoroutineBehaviour : MonoBehaviour
 {
-    public UnityEvent repeatEvent;
+    public UnityEvent startEvent, repeatEvent, endEvent;
 
-    public int counterNum = 3;
+    public IntData counterNum = 3;
     public float seconds = 3.0f;
     private WaitForSeconds wfsObj;
     private WaitForFixedUpdate wffuObj;
@@ -16,14 +16,14 @@ public class CoroutineBehaviour : MonoBehaviour
     {
         wfsObj = new WaitForSeconds(seconds);
         wffuObj = new WaitForFixedUpdate();
-
-        while (counterNum > 0)
+        startEvent.Invoke();
+        yield return wfsObj;
+        while (counterNum.value > 0)
         {
-            Debug.Log(counterNum);
-            yield return wfsObj;
             repeatEvent.Invoke();
-            counterNum--;
-            
+            counterNum.value--;
+            yield return wfsObj;
         }
+        endEvent.Invoke();
     }
 }
