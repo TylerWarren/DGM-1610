ab5dd8e9a08877372934a70c3a1062f7e7efccea
Tyler Warren <10937681@my.uvu.edu>
2022-11-23 13:42:28 -0700

    updated

--- UnityProjects/Color_Match_Game/Assets/Scripts/CoroutineBehaviour.cs
+++ UnityProjects/Color_Match_Game/Assets/Scripts/CoroutineBehaviour.cs
@@ -1,56 +1,29 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
-
-public class CoroutineBehaviour : MonoBehaviour
+ 
+public class CoroutineBehaviour2 : MonoBehaviour
 {
-    public UnityEvent startEvent, startCountEvent, repeatCountEvent, endCountEvent,RepeatUntilFalseEvent;
-
-    public bool canRun;
+    public UnityEvent startEvent, repeatEvent, endEvent;
+ 
     public IntData counterNum;
     public float seconds = 3.0f;
     private WaitForSeconds wfsObj;
     private WaitForFixedUpdate wffuObj;
-
-    private void Start()
-    {
-        startEvent.Invoke();
-    }
-
-    public void StartCounting()
-    {
-        StartCoroutine(Counting());
-    }
-
-    private IEnumerator Counting()
+ 
+    IEnumerator Start()
     {
         wfsObj = new WaitForSeconds(seconds);
         wffuObj = new WaitForFixedUpdate();
-        startCountEvent.Invoke();
+        startEvent.Invoke();
         yield return wfsObj;
         while (counterNum.value > 0)
         {
-            repeatCountEvent.Invoke();
+            repeatEvent.Invoke();
             counterNum.value--;
             yield return wfsObj;
         }
-        endCountEvent.Invoke();
-    }
-
-    public void StartRepeatUntilFalse()
-    {
-        canRun = true;
-        StartCoroutine(RepeatUntilFalse());
-    }
-
-
-    private IEnumerator RepeatUntilFalse()
-    {
-        while (canRun)
-        {
-            yield return wfsObj;
-            RepeatUntilFalseEvent.Invoke();
-        }
-
+        endEvent.Invoke();
     }
 }
