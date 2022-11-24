b067ec3522e1d2eb7378f19f585690c5c30ebe89
Tyler Warren <10937681@my.uvu.edu>
2022-11-23 22:50:32 -0700

    updated code

--- UnityProjects/Color_Match_Game/Assets/Scripts/ColorIDDataList.cs
+++ UnityProjects/Color_Match_Game/Assets/Scripts/ColorIDDataList.cs
@@ -11,7 +11,7 @@ public class ColorIDDataList : ScriptableObject
 
     public void SetCurrentColorRandomly()
     {
-        num = colorIDList.Count-1;
+        num = Random.Range(0, colorIDList.Count);
         currentColor = colorIDList[num];
     }
 }
