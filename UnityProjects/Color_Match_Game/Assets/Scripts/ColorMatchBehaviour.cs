bd0c76470805ef7333584f40c4371eef1c94c48f
Tyler Warren <10937681@my.uvu.edu>
2022-11-24 20:45:16 -0700

    fixed error

--- UnityProjects/Color_Match_Game/Assets/Scripts/ColorMatchBehaviour.cs
+++ UnityProjects/Color_Match_Game/Assets/Scripts/ColorMatchBehaviour.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ColorMatchBehaviour : MatchBehaviour
+public class ColorMatchBehaviour : MatchBehavior
 {
     public ColorIDDataList colorIDDataListObj;
 
